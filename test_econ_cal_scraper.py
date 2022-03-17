66078fac3c71f3bdd7c7e014016888b0365efc7d
Ghulam  Jatoi <aj8@Ghulams-MacBook-Pro.local>
2022-03-17 03:10:15 +0000

    Functionality for downloading images using urlretrieve. However website responds with forbidden 403 image

---
+++ test_econ_cal_scraper.py
@@ -0,0 +1,19 @@
+import unittest
+from econ_cal_scraper import EconCalScraper
+
+class EconCalScraperTestCase(unittest.TestCase):
+    #def setUp(self) -> None:
+    #    return super().setUp():
+    pass
+
+    def test_getPage(self):
+        pass
+
+    def test_quitScrap(self):
+        pass
+
+    def test_getLinks(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
